838c2c1bd3ceabdf3375018d8cc0851deaadf702
Yangxxxxx <1466154287@qq.com>
2017-07-18 08:36:59 +0800

    github上传代码测试3

--- app/src/main/java/com/example/yangjitao/icodestore/drawableTest/DrawableTestActivity.java
+++ app/src/main/java/com/example/yangjitao/icodestore/drawableTest/DrawableTestActivity.java
@@ -16,9 +16,6 @@ public class DrawableTestActivity extends Activity implements View.OnClickListen
         setContentView(R.layout.activity_drawable_test);
         findViewById(R.id.bt_select).setOnClickListener(this);
 
-        // git 测试 local
-
-        //git测试
         //设置监听才有点击效果
         findViewById(R.id.one_pic).setOnClickListener(this);
     }
