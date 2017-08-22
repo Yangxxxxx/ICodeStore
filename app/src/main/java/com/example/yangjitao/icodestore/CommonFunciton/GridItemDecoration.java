a0e85a87104073746f58b47ccde54a516f6c7db7
yangxxxxx <1466154287@qq.com>
2017-08-22 14:07:53 +0800

    fragment 使用相关代码

---
+++ app/src/main/java/com/example/yangjitao/icodestore/CommonFunciton/GridItemDecoration.java
@@ -0,0 +1,98 @@
+package com.example.yangjitao.icodestore.CommonFunciton;
+
+import android.content.Context;
+import android.graphics.Rect;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.RecyclerView.LayoutManager;
+
+
+public class GridItemDecoration extends RecyclerView.ItemDecoration {
+
+    private int offTop = 0;
+    private int offBottom = 0;
+    private int offLeft = 0;
+    private int offRight = 0;
+    int interval = 20;
+
+    public int getOffTop(){
+        return offTop;
+    }
+
+    public GridItemDecoration() {
+    }
+
+    public GridItemDecoration(Context context, int interval) {
+        this.interval = interval;
+    }
+
+    public GridItemDecoration(Context context, int offLeft, int offTop, int offRight, int offBottom, int interval) {
+        this.offBottom = offBottom;
+        this.offTop = offTop;
+        this.offLeft = offLeft;
+        this.offRight = offRight;
+        this.interval = interval;
+    }
+
+
+    @Override
+    public void getItemOffsets(Rect outRect, int itemPosition,
+                               RecyclerView parent) {
+        int spanCount = getSpanCount(parent);
+        int childCount = parent.getAdapter().getItemCount();
+
+        int left = interval / 2;
+        int top = interval / 2;
+        int right = interval / 2;
+        int bottom = interval / 2;
+
+        if (isFirstRaw(itemPosition, spanCount)) {
+            top = offTop;
+        }
+        if (isFirstColumn(itemPosition, spanCount)) {
+            left = offLeft;
+        }
+        if (isLastRaw(itemPosition, spanCount, childCount)) {
+            bottom = offBottom;
+        }
+        if (isLastColum(itemPosition, spanCount)) {
+            right = offRight;
+        }
+        outRect.set(left, top, right, bottom);
+    }
+
+    private int getSpanCount(RecyclerView parent) {
+        LayoutManager layoutManager = parent.getLayoutManager();
+        if (layoutManager instanceof GridLayoutManager) {
+            return  ((GridLayoutManager) layoutManager).getSpanCount();
+        }
+        return 0;
+    }
+
+
+    private boolean isLastColum(int pos, int spanCount) {
+        return (pos + 1) % spanCount == 0;
+    }
+
+    private boolean isLastRaw(int pos, int spanCount, int childCount) {
+        int remainder = childCount % spanCount;
+
+        /** 最后一行的起始位置*/
+        int lastRawFirstPos;
+        if (remainder == 0) {
+            lastRawFirstPos = childCount - spanCount;
+        } else {
+            lastRawFirstPos = childCount - remainder;
+        }
+        return (pos >= lastRawFirstPos);
+    }
+
+
+    private boolean isFirstRaw(int itemPosition, int spanCount) {
+        return itemPosition < spanCount;
+    }
+
+    private boolean isFirstColumn(int itemPosition, int spanCount) {
+        return itemPosition % spanCount == 0;
+    }
+}
--- app/src/main/java/com/example/yangjitao/icodestore/recycleviewTest/RecycleViewActivity.java
+++ app/src/main/java/com/example/yangjitao/icodestore/recycleviewTest/RecycleViewActivity.java
@@ -1,17 +1,15 @@
 package com.example.yangjitao.icodestore.recycleviewTest;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
-import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ImageView;
 
-import com.example.yangjitao.icodestore.CommonFunciton.DividerGridItemDecoration;
+import com.example.yangjitao.icodestore.CommonFunciton.GridItemDecoration;
 import com.example.yangjitao.icodestore.R;
 
 public class RecycleViewActivity extends AppCompatActivity {
@@ -29,10 +27,10 @@ public class RecycleViewActivity extends AppCompatActivity {
 //        cateLayoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
 
         Log.e("yang", "enter onCreate");
-        recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 6));
 
-        recyclerView.addItemDecoration(new DividerGridItemDecoration(this));
-
+        recyclerView.addItemDecoration(new GridItemDecoration(this, 0, 0, 0, 0, 20));
+//
         HomeAdapter cateAdapter = new HomeAdapter();
         recyclerView.setAdapter(cateAdapter);
     }
@@ -44,94 +42,35 @@ public class RecycleViewActivity extends AppCompatActivity {
         @Override
         public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType)
         {
-//            MyViewHolder holder = new MyViewHolder(LayoutInflater.from(RecycleViewActivity.this).inflate(R.layout.recycleview_list_item, parent, false));
-            TextView textView = new TextView(parent.getContext());
-            ViewGroup.LayoutParams layoutParams = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 200);
-            textView.setLayoutParams(layoutParams);
-            textView.setBackgroundColor(Color.WHITE);
-            textView.setGravity(Gravity.CENTER);
-
-            return new MyViewHolder(textView);
+            ImageView imageView = new ImageView(parent.getContext());
+            ViewGroup.LayoutParams layoutParams = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 300);
+            imageView.setLayoutParams(layoutParams);
+            imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+            return new MyViewHolder(imageView);
         }
         @Override
         public void onBindViewHolder(MyViewHolder holder, int position)
         {
-            holder.textView.setText(position+"");
+            holder.imageView.setImageResource(R.mipmap.img);
         }
 
         @Override
         public int getItemCount()
         {
-            return 39;
+            return 139;
         }
 
         class MyViewHolder extends RecyclerView.ViewHolder
         {
 
-            TextView textView;
+            ImageView imageView;
 
             public MyViewHolder(View view)
             {
                 super(view);
-                textView = (TextView) view;
+                imageView = (ImageView)view;
             }
         }
     }
 
-
-
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_recycleview);
-//
-//        recyclerView = (RecyclerView) findViewById(R.id.rcv_filters);
-//        LinearLayoutManager cateLayoutManager = new LinearLayoutManager(this);
-//        cateLayoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
-//        recyclerView.setLayoutManager(cateLayoutManager);
-//        HomeAdapter cateAdapter = new HomeAdapter();
-//        recyclerView.setAdapter(cateAdapter);
-//    }
-//
-//
-//    class HomeAdapter extends RecyclerView.Adapter<HomeAdapter.MyViewHolder>
-//    {
-//
-//        int[] previewID = new int[]{R.mipmap.ic_launcher, R.mipmap.ic_launcher, R.mipmap.ic_launcher,
-//                R.mipmap.ic_launcher, R.mipmap.ic_launcher, R.mipmap.ic_launcher,
-//                R.mipmap.ic_launcher, R.mipmap.ic_launcher, R.mipmap.ic_launcher};
-//
-//        public HomeAdapter() {
-//        }
-//
-//        @Override
-//        public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType)
-//        {
-//            MyViewHolder holder = new MyViewHolder(LayoutInflater.from(RecycleViewActivity.this).inflate(R.layout.recycleview_list_item, parent, false));
-//            return holder;
-//        }
-//        @Override
-//        public void onBindViewHolder(MyViewHolder holder, int position)
-//        {
-//            holder.iv.setImageResource(previewID[position]);
-//        }
-//
-//        @Override
-//        public int getItemCount()
-//        {
-//            return previewID.length;
-//        }
-//
-//        class MyViewHolder extends RecyclerView.ViewHolder
-//        {
-//
-//            ImageView iv;
-//
-//            public MyViewHolder(View view)
-//            {
-//                super(view);
-//                iv = (ImageView) view.findViewById(R.id.imageView);
-//            }
-//        }
-//    }
 }
