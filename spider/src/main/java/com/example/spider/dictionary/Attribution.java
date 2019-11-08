a47a45003216f7c66ca3f8c0288ac3c157eafa82
yangxxxxx <1466154287@qq.com>
2019-11-08 21:09:26 +0800

    spider: 完善爬虫程序demo

--- spider/src/main/java/com/example/spider/dictionary/Attribution.java
+++ spider/src/main/java/com/example/spider/dictionary/Attribution.java
@@ -6,7 +6,7 @@ import com.example.spider.annotation.FieldPath;
 
 public class Attribution {
     @FieldPath("pos dpos")
-    public String wordAttr;
+    public String wordAttr; //词性：verb、noun等
 
     @FieldPath("def-block ddef_block ")
     public Translation[] translations;
